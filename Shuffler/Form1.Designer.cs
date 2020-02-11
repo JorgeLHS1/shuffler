4be4a656d00629e0ed408af582afecef812d3e6f
Jorge_LHS <jorge_lhs@live.com>
2020-02-11 20:46:05 -0300

    first commit

---
+++ Shuffler/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Shuffler
+{
+    partial class Shuffler
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.columnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnShuffler = new System.Windows.Forms.Button();
+            this.btnUnshuffler = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 26);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(125, 42);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Selecionar pasta";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnName,
+            this.columnPath});
+            this.dataGridView.Location = new System.Drawing.Point(12, 74);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(600, 355);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // columnName
+            // 
+            this.columnName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.columnName.HeaderText = "Nome";
+            this.columnName.Name = "columnName";
+            this.columnName.ReadOnly = true;
+            this.columnName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.columnName.Width = 555;
+            // 
+            // columnPath
+            // 
+            this.columnPath.HeaderText = "Path";
+            this.columnPath.Name = "columnPath";
+            this.columnPath.ReadOnly = true;
+            this.columnPath.Visible = false;
+            // 
+            // btnShuffler
+            // 
+            this.btnShuffler.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShuffler.Location = new System.Drawing.Point(356, 26);
+            this.btnShuffler.Name = "btnShuffler";
+            this.btnShuffler.Size = new System.Drawing.Size(125, 42);
+            this.btnShuffler.TabIndex = 2;
+            this.btnShuffler.Text = "Embaralhar músicas";
+            this.btnShuffler.UseVisualStyleBackColor = true;
+            this.btnShuffler.Click += new System.EventHandler(this.btnShuffler_Click);
+            // 
+            // btnUnshuffler
+            // 
+            this.btnUnshuffler.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUnshuffler.Location = new System.Drawing.Point(487, 26);
+            this.btnUnshuffler.Name = "btnUnshuffler";
+            this.btnUnshuffler.Size = new System.Drawing.Size(125, 42);
+            this.btnUnshuffler.TabIndex = 3;
+            this.btnUnshuffler.Text = "Desembaralhar músicas";
+            this.btnUnshuffler.UseVisualStyleBackColor = true;
+            this.btnUnshuffler.Click += new System.EventHandler(this.btnUnshuffler_Click);
+            // 
+            // Shuffler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.btnUnshuffler);
+            this.Controls.Add(this.btnShuffler);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.btnSelectFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "Shuffler";
+            this.Text = "Shuffler";
+            this.Load += new System.EventHandler(this.Shuffler_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnShuffler;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnPath;
+        private System.Windows.Forms.Button btnUnshuffler;
+    }
+}
+
---
+++ Shuffler/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shuffler
+{
+    public partial class Shuffler : Form
+    {
+        public Shuffler()
+        {
+            InitializeComponent();
+        }
+        List<String> listPaths = new List<String>();
+        String selectedPath;
+
+        private void Shuffler_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        void DirSearch(string sDir)
+        {
+            try
+            {
+                dataGridView.Rows.Clear();
+                listPaths.Clear();
+
+                foreach (string f in GetFiles(sDir, "*.mp3|*.wav|*.acc|*.pcm|*.aiff|*.ogg|*.wma|*.flac|.alac"))
+                {
+                    Console.WriteLine(f);
+                    dataGridView.Rows.Add(f, f);
+                    listPaths.Add(f);
+                }
+            }
+            catch (Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+
+
+        public void ChooseFolder()
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedPath = folderBrowserDialog.SelectedPath;
+                Console.WriteLine(selectedPath);
+                DirSearch(selectedPath);
+            }
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            ChooseFolder();
+        }
+
+        private void btnShuffler_Click(object sender, EventArgs e)
+        {
+            UnshufflerMusic();
+            ShufflerMusic();
+
+        }
+
+        public string[] GetFiles(string SourceFolder, string Filter)
+        {
+            // ArrayList will hold all file names
+            ArrayList alFiles = new ArrayList();
+
+            // Create an array of filter string
+            string[] MultipleFilters = Filter.Split('|');
+
+            // for each filter find mathing file names
+            foreach (string FileFilter in MultipleFilters)
+            {
+                // add found file names to array list
+                alFiles.AddRange(Directory.GetFiles(SourceFolder, FileFilter));
+            }
+
+            // returns string array of relevant file names
+            return (string[])alFiles.ToArray(typeof(string));
+        }
+
+        private void ShufflerMusic()
+        {
+            int count = dataGridView.Rows.Count;
+            Random rand = new Random();
+            List<int> listNumbers = new List<int>();
+            int number;
+            for (int i = 1; i < count; i++)
+            {
+                do
+                {
+                    number = rand.Next(1, count);
+                } while (listNumbers.Contains(number));
+                listNumbers.Add(number);
+            }
+            foreach (int i in listNumbers)
+            {
+                string fileName;
+                if (selectedPath.EndsWith(":\\"))
+                {
+                    fileName = listPaths[i - 1].Substring(selectedPath.Length);
+                }
+                else
+                {
+                    fileName = listPaths[i - 1].Substring(selectedPath.Length + 1);
+                }
+
+                if (Math.Floor(Math.Log10(listNumbers[i - 1]) + 1) == 1)
+                {
+                    File.Move(listPaths[i - 1], selectedPath + "\\" + "0000" + listNumbers[i - 1] + "%-%" + fileName);
+                    Console.WriteLine("0000" + listNumbers[i - 1] + "%-%" + fileName);
+                }
+                else if (Math.Floor(Math.Log10(listNumbers[i - 1]) + 1) == 2)
+                {
+                    File.Move(listPaths[i - 1], selectedPath + "\\" + "000" + listNumbers[i - 1] + " %-%" + fileName);
+                    Console.WriteLine("000" + listNumbers[i - 1] + "%-%" + fileName);
+                }
+                else if (Math.Floor(Math.Log10(listNumbers[i - 1]) + 1) == 3)
+                {
+                    File.Move(listPaths[i - 1], selectedPath + "\\" + "00" + listNumbers[i - 1] + "%-%" + fileName);
+                    Console.WriteLine("00" + listNumbers[i - 1] + "%-%" + fileName);
+                }
+                else if (Math.Floor(Math.Log10(listNumbers[i - 1]) + 1) == 4)
+                {
+                    File.Move(listPaths[i - 1], selectedPath + "\\" + "0" + listNumbers[i - 1] + "%-%" + fileName);
+                    Console.WriteLine("0" + listNumbers[i - 1] + "%-%" + fileName);
+                }
+                else
+                {
+                    File.Move(listPaths[i - 1], selectedPath + "\\" +listNumbers[i - 1] + "%-%" + fileName);
+                    Console.WriteLine(listNumbers[i - 1] + "%-%" + fileName);
+                }
+
+            }
+            DirSearch(selectedPath);
+        }
+
+
+        private void UnshufflerMusic()
+        {
+            int count = dataGridView.Rows.Count;
+            for (int i = 1; i < count; i++)
+            {
+                string fileName;
+                if (selectedPath.EndsWith(":\\"))
+                {
+                    fileName = listPaths[i - 1].Substring(selectedPath.Length);
+                }
+                else
+                {
+                    fileName = listPaths[i - 1].Substring(selectedPath.Length + 1);
+                }
+                if (listPaths[i - 1].Contains("%-%"))
+                {
+                    fileName = "\\" + fileName.Substring(fileName.IndexOf("%-%") + 3);
+                    File.Move(listPaths[i - 1], selectedPath + fileName);
+                    Console.WriteLine(selectedPath + fileName);
+                }
+            }
+            DirSearch(selectedPath);
+        }
+
+        private void btnUnshuffler_Click(object sender, EventArgs e)
+        {
+            UnshufflerMusic();
+
+        }
+    }
+}
